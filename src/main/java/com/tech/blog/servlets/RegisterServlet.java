4c766761b97c17ce428805c47e90763cb6de60d7
Rehbar12 <mohdrehbar4@gmail.com>
2023-02-20 21:12:59 +0530

    first commit

---
+++ src/main/java/com/tech/blog/servlets/RegisterServlet.java
@@ -0,0 +1,46 @@
+package com.tech.blog.servlets;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.*;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+import com.tech.blog.dao.UserDao;
+import com.tech.blog.entities.User;
+import com.tech.blog.helper.ConnectionProvider;
+@SuppressWarnings("serial")
+@MultipartConfig
+@WebServlet("/RegisterServlet")
+
+public class RegisterServlet  extends HttpServlet{
+	public void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException{
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		String check=request.getParameter("check");
+		if(check==null)
+			out.println("Please Check Term and Condation");
+		else{
+			String name=request.getParameter("user_name");
+			String email=request.getParameter("user_email");
+			String password=request.getParameter("user_password");
+			String gender=request.getParameter("gender");
+			String about=request.getParameter("about");
+			
+			//create object of User to set all data 
+			User user=new User(name,email,password,about,gender);
+			
+			//create object of UserDao to send data into database
+			
+			UserDao dao=new UserDao(ConnectionProvider.getConnection());
+			if(dao.saveUser(user)){
+				out.println("done");
+			}
+			else{
+				out.println("error");
+			}
+		}
+		
+	}
+
+}
